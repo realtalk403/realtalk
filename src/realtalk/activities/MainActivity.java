a826fd667487afcb56080be32dda30c886c46069
TaylorW <Taylor@173.250.159.109>
2013-04-30 16:16:27 -0700

    (sort of) Abstracted ChatManager usage pattern

--- src/realtalk/activities/MainActivity.java
+++ src/realtalk/activities/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
     private ProgressDialog pDialog;
     
 	@Override
-	protected void onCreate(Bundle savedInstanceState) { 
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
--- src/realtalk/util/ChatManager.java
+++ src/realtalk/util/ChatManager.java
@@ -16,22 +16,25 @@ public class ChatManager {
     public static final String url_remove_user = "http://realtalkserver.herokuapp.com/unregister";
     public static final String url_authenticate = "http://realtalkserver.herokuapp.com/authenticate";
     public static final String TAG_SUCCESS = "success";
+    
+    private static JSONObject makePostRequest(List<NameValuePair> rgParams, String url)
+    {
+    	JSONObject json = null;
+    	JSONParser jsonParser = new JSONParser();
+		json = jsonParser.makeHttpRequest(url, "POST", rgParams);
+    	return json;
+    }
 	
 	public static boolean authenticateUser(User user) {
         boolean fAuthenticated;
+        List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
+        rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
+        rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
+        rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
+        JSONObject json = makePostRequest(rgParams, url_authenticate);
         try {
-            List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
-            rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
-            rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
-            rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
-            
-            // Make http request to obtain results
-            JSONParser jsonParser = new JSONParser();
-            JSONObject json = jsonParser.makeHttpRequest(url_authenticate, "POST", rgParams);
-            
             fAuthenticated = json.getString(TAG_SUCCESS).equals("true");
             return fAuthenticated;
-            
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -39,20 +42,15 @@ public class ChatManager {
 	}
 	
 	public static boolean addUser(User user) {
-        boolean fAddSucceeded;
+        boolean fAdded;
+        List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
+        rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
+        rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
+        rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
+        JSONObject json = makePostRequest(rgParams, url_add_user);
         try {
-            List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
-            rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
-            rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
-            rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
-            
-            // Make http request to obtain results
-            JSONParser jsonParser = new JSONParser();
-            JSONObject json = jsonParser.makeHttpRequest(url_add_user, "POST", rgParams);
-            
-            fAddSucceeded = json.getString("PARAMETER_REG_ID").equals(user.id);
-            return fAddSucceeded;
-            
+            fAdded = json.getString(TAG_SUCCESS).equals("true");
+            return fAdded;
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -60,20 +58,15 @@ public class ChatManager {
 	}
 	
 	public static boolean removeUser(User user) {
-        boolean fRemovalSucceeded;
+        boolean fRemoved;
+        List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
+        rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
+        rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
+        rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
+        JSONObject json = makePostRequest(rgParams, url_remove_user);
         try {
-            List<NameValuePair> rgParams = new ArrayList<NameValuePair>();
-            rgParams.add(new BasicNameValuePair("PARAMETER_REG_ID", user.id));
-            rgParams.add(new BasicNameValuePair("PARAMETER_USER", user.username));
-            rgParams.add(new BasicNameValuePair("PARAMETER_PWORD", user.password));
-            
-            // Make http request to obtain results
-            JSONParser jsonParser = new JSONParser();
-            JSONObject json = jsonParser.makeHttpRequest(url_remove_user, "POST", rgParams);
-            
-            fRemovalSucceeded = json.getString("PARAMETER_REG_ID").equals(user.id);
-            return fRemovalSucceeded;
-            
+            fRemoved = json.getString(TAG_SUCCESS).equals("true");
+            return fRemoved;
         } catch (JSONException e) {
             e.printStackTrace();
         }
