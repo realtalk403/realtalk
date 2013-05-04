8f839d31688ec9f712e53712a77b4397ceed8a0e
Brandon Lee <leeb92@uw.edu>
2013-05-04 15:54:51 -0700

    Integrated logo, updated UI, added pop up dialogs.

--- src/realtalk/activities/MainActivity.java
+++ src/realtalk/activities/MainActivity.java
@@ -33,94 +33,133 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	/**
+	 * Called when user clicks "Create Account".  Redirects to Create Account page.
+	 * 
+	 * @param view
+	 */
 	public void addUser(View view) {
-//	    EditText edittextUser = (EditText) findViewById(R.id.editQuery);
-//	    EditText edittextPword = (EditText) findViewById(R.id.editPword);
-//	    String stUsername = edittextUser.getText().toString();
-//	    String stPword = edittextPword.getText().toString();
-//	    new UserAdder(new User(stUsername, stPword)).execute();
 		Intent createAcc = new Intent(this, CreateAccountActivity.class);
 		this.startActivity(createAcc);
 	}
 	
+	/** 
+	 * Called when user attempts to login.
+	 * 
+	 * @param view
+	 */
 	public void authenticateUser(View view) {
 	    EditText edittextUser = (EditText) findViewById(R.id.editQuery);
 	    EditText edittextPword = (EditText) findViewById(R.id.editPword);
 	    String stUsername = edittextUser.getText().toString();
 	    String stPword = edittextPword.getText().toString();
-	    new Authenticator(new User(stUsername, stPword), this).execute();
+	    
+	    if(!stUsername.equals("") && !stUsername.equals("")) {
+	    	new Authenticator(new User(stUsername, stPword), this).execute();
+	    } else {
+	    	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			//set title
+			alertDialogBuilder.setTitle("Invalid input");
+			
+			//set dialog message
+			alertDialogBuilder
+				.setMessage("Please enter a username & password.")
+				.setCancelable(false);
+			
+			alertDialogBuilder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					// User cancelled the dialog
+					dialog.cancel();
+				}
+			});
+			
+			//create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			
+			//show alert dialog
+			alertDialog.show();	
+	    }
+	    
 	}
 	
+	/**
+	 * Called when user attempts to delete their account.
+	 * 
+	 * @param view
+	 */
 	public void removeUser(View view) {
-		//confirmation pop up
-    	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		//set title
-		alertDialogBuilder.setTitle("Unregister account");
-		
-		//set dialog message
-		alertDialogBuilder
-			.setMessage("Are you sure you want to unregister this account?")
-			.setCancelable(false);
-		
-		alertDialogBuilder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				//close the dialog box if this button is clicked
-				EditText edittextUser = (EditText) findViewById(R.id.editQuery);
-			    EditText edittextPword = (EditText) findViewById(R.id.editPword);
-			    String stUsername = edittextUser.getText().toString();
-			    String stPword = edittextPword.getText().toString();
-			    new UserRemover(new User(stUsername, stPword)).execute();
-			}	
-		});
-		
-		alertDialogBuilder.setPositiveButton("No", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				// User cancelled the dialog
-				dialog.cancel();
-			}
-		});
-		
-		//create alert dialog
-		AlertDialog alertDialog = alertDialogBuilder.create();
-		
-		//show alert dialog
-		alertDialog.show();	
+		EditText edittextUser = (EditText) findViewById(R.id.editQuery);
+	    EditText edittextPword = (EditText) findViewById(R.id.editPword);
+	    String stUsername = edittextUser.getText().toString();
+	    String stPword = edittextPword.getText().toString();
+		if(stUsername.equals("") || stPword.equals("")) {
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			//set title
+			alertDialogBuilder.setTitle("Invalid input");
+			
+			//set dialog message
+			alertDialogBuilder
+				.setMessage("Please enter a username & password.")
+				.setCancelable(false);
+			
+			
+			alertDialogBuilder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					// User cancelled the dialog
+					dialog.cancel();
+				}
+			});
+			
+			//create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			
+			//show alert dialog
+			alertDialog.show();
+		} else {
+			//confirmation pop up
+	    	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			//set title
+			alertDialogBuilder.setTitle("Delete account");
+			
+			//set dialog message
+			alertDialogBuilder
+				.setMessage("Are you sure you want to delete this account?")
+				.setCancelable(false);
+			
+			alertDialogBuilder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					//close the dialog box if this button is clicked
+					EditText edittextUser = (EditText) findViewById(R.id.editQuery);
+				    EditText edittextPword = (EditText) findViewById(R.id.editPword);
+				    String stUsername = edittextUser.getText().toString();
+				    String stPword = edittextPword.getText().toString();
+				    new UserRemover(new User(stUsername, stPword), MainActivity.this).execute();
+				}	
+			});
+			
+			alertDialogBuilder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					// User cancelled the dialog
+					dialog.cancel();
+				}
+			});
+			
+			//create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			
+			//show alert dialog
+			alertDialog.show();	
+		}
 	}
-	
-//	class UserAdder extends AsyncTask<String, String, RequestResultSet> {
-//		private User user;
-//		public UserAdder(User user) {
-//			this.user = user;
-//		}
-//		
-//	    @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//            pDialog = new ProgressDialog(MainActivity.this);
-//            pDialog.setMessage("Loading user details. Please wait...");
-//            pDialog.setIndeterminate(false);
-//            pDialog.setCancelable(true);
-//            pDialog.show();
-//        }
-//	    
-//        @Override
-//        protected RequestResultSet doInBackground(String... params) {
-//        	return ChatManager.addUser(user);
-//        }
-//        
-//        @Override
-//        protected void onPostExecute(RequestResultSet requestresultset) {
-//            pDialog.dismiss();
-//            TextView addingResults = (TextView) findViewById(R.id.query_results_textView);
-//            addingResults.setText(requestresultset.stErrorMessage);
-//        }
-//	}
+
 
 	class UserRemover extends AsyncTask<String, String, RequestResultSet> {
 		private User user;
-		public UserRemover(User user) {
+		private Activity activity;
+		public UserRemover(User user, Activity activity) {
 			this.user = user;
+			this.activity = activity;
 		}
 		
 	    @Override
@@ -141,8 +180,57 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(RequestResultSet requestresultset) {
             pDialog.dismiss();
-            TextView removalResults = (TextView) findViewById(R.id.query_results_textView);
-            removalResults.setText(requestresultset.stErrorMessage);
+            if(requestresultset.fSucceeded == false) {
+            	//invalid username or password
+            	
+            	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+				//set title
+				alertDialogBuilder.setTitle("Invalid fields");
+				
+				//set dialog message
+				alertDialogBuilder
+					.setMessage("Invalid username/password. Please try again.")
+					.setCancelable(false)
+					.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							//close the dialog box if this button is clicked
+							dialog.cancel();
+						}	
+				});
+				
+				//create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				
+				//show alert dialog
+				alertDialog.show();	
+            } else {
+            	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+				//set title
+				alertDialogBuilder.setTitle("Account deleted");
+				
+				//set dialog message
+				alertDialogBuilder
+					.setMessage("Your account has been deleted.")
+					.setCancelable(false)
+					.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							//close the dialog box if this button is clicked
+							dialog.cancel();
+						}	
+				});
+				
+				//create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				
+				//show alert dialog
+				alertDialog.show();	
+				
+				TextView username = (TextView) findViewById(R.id.editQuery);
+	            username.setText("");
+            }
+            
+            TextView password = (TextView) findViewById(R.id.editPword);
+            password.setText("");
         }
 	}
 	
@@ -152,6 +240,7 @@ public class MainActivity extends Activity {
 		
 		public Authenticator(User user, Activity activity) {
 			this.user = user;
+			this.activity = activity;
 		}
 		
 	    @Override
@@ -172,8 +261,55 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(RequestResultSet requestresultset) {
             pDialog.dismiss();       
-            TextView authenticationResults = (TextView) findViewById(R.id.query_results_textView);
-            authenticationResults.setText(requestresultset.stErrorMessage);
+            //invalid username/password
+            if(requestresultset.fSucceeded == false) {
+            	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+				//set title
+				alertDialogBuilder.setTitle("Invalid fields");
+				
+				//set dialog message
+				alertDialogBuilder
+					.setMessage("Invalid username/password. Please try again.")
+					.setCancelable(false)
+					.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							//close the dialog box if this button is clicked
+							dialog.cancel();
+						}	
+				});
+				
+				//create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				
+				//show alert dialog
+				alertDialog.show();	
+				
+				TextView password = (TextView) findViewById(R.id.editPword);
+	            password.setText("");
+            } else {
+            	//PUT WHATEVER CODE FOR A SUCCESSFUL LOGIN HERE
+            	
+            	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+				//set title
+				alertDialogBuilder.setTitle("Login successful");
+				
+				//set dialog message
+				alertDialogBuilder
+					.setMessage("Login Successful!")
+					.setCancelable(false)
+					.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							//close the dialog box if this button is clicked
+							dialog.cancel();
+						}	
+				});
+				
+				//create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				
+				//show alert dialog
+				alertDialog.show();	
+            }
         }
 	}
 }
