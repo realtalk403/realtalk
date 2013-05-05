137a7d8f2dad38b8f4faef3a14d033c009e69601
Jory Rice <thisisjory@gmail.com>
2013-05-05 15:13:36 -0700

    Added UML server class diagram

---
+++ realtalk-master/src/realtalk/util/User.java
@@ -0,0 +1,12 @@
+package realtalk.util;
+
+public class User {
+	String id;
+	String username;
+	String password; //TODO should this be here?
+	public User(String id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+}
