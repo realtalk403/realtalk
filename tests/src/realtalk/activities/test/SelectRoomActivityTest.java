033e860fbda84a960d92ebc7068f4bc6ae67dceb
Jory Rice <thisisjory@gmail.com>
2013-05-19 15:38:50 -0700

    Fixed delete acconut bug (round 2)

---
+++ tests/src/realtalk/activities/test/SelectRoomActivityTest.java
@@ -0,0 +1,23 @@
+package realtalk.activities.test;
+
+import static org.junit.Assert.*;
+import realtalk.activities.*;
+
+import org.junit.Test;
+
+import android.annotation.SuppressLint;
+import android.test.ActivityInstrumentationTestCase2;
+
+@SuppressLint("NewApi") public class SelectRoomActivityTest extends ActivityInstrumentationTestCase2<SelectRoomActivity> {
+
+	public SelectRoomActivityTest(Class<SelectRoomActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
