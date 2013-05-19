06ea959d0071defb5a15ec00c8a913b94d858fde
TaylorW <Taylor@173.250.159.109>
2013-05-18 20:02:01 -0700

    Added test for PullMessageResultSet

---
+++ tests/src/realtalk/util/PullMessageResultSetTest.java
@@ -0,0 +1,63 @@
+package realtalk.util;
+
+import org.junit.Test;
+import realtalk.util.PullMessageResultSet;
+
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import android.test.AndroidTestCase;
+/**
+ * PullMessageResult Test
+ * 
+ * White Box Test Case.
+ * 
+ * @author Taylor Williams
+ *
+ */
+public class PullMessageResultSetTest extends AndroidTestCase {
+    private PullMessageResultSet pmrsSuccess;
+    private PullMessageResultSet pmrsFailure;
+    private static final int TIMEOUT = 10000;
+    private static final String stErrorCode = "ERRORCODE";
+    private static final String stErrorMessage = "ERRORMESSAGE";
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        pmrsFailure = new PullMessageResultSet(false, stErrorMessage, stErrorCode);
+        pmrsSuccess = new PullMessageResultSet(true, new ArrayList<MessageInfo>(), "NONE", "NONE");
+    }
+    
+    @Test(timeout = TIMEOUT)
+    public void testSuccessionGetter() {
+        assertTrue(!pmrsFailure.fIsSucceeded());
+        assertTrue(pmrsSuccess.fIsSucceeded());
+    }
+    
+    @Test(timeout = TIMEOUT)
+    public void testMessagesGetter() {
+    	//will cause a null pointer exception if constructor failed
+        assertTrue(pmrsSuccess.getRgmessage().size() == 0);
+    }
+    
+    @Test(timeout = TIMEOUT)
+    public void testErrorMessageGetter() {
+        assertTrue(pmrsFailure.getStErrorMessage().equals(stErrorMessage));
+    }
+    
+    @Test(timeout = TIMEOUT)
+    public void testErrorCodeGetter() {
+    	assertTrue(pmrsFailure.getStErrorCode().equals(stErrorCode));
+    }
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
