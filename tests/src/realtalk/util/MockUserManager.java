bd44b91c639507fd16a8d2c5b57f98ef8dab65cb
Colin Kho(HP) <colinzkho@hotmail.com>
2013-05-19 17:41:56 -0700

    Added TDD tests

---
+++ tests/src/realtalk/util/MockUserManager.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package realtalk.util;
+
+/**
+ * UserManager is a singleton which keeps track of RealTalk's current user and provides
+ * methods that allow logging out, logging in and managing the registration id of the user.
+ * It is discouraged to call it directly and should be accessed through the ChatController.
+ * 
+ * @author Colin Kho
+ *
+ */
+public class MockUserManager {
+	private MockUserManager userControllerSingleton = 
+			null;
+	
+	private MockUserManager() { }
+	
+	/**
+	 * Gets a singleton instance of the UserManager
+	 * 
+	 * @return current instance of UserManager
+	 */
+	public static MockUserManager getInstance() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean fLogin(String name, String password) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean fLogout() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean fChangeUser(String name, String password) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String stGetUserName() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String stGetPassword() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String stGetRegId() {
+		throw new UnsupportedOperationException();
+	}
+}
