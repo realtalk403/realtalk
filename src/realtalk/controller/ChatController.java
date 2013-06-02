50e63ce9954ee096ca6d6f2b794b6b31b62d9406
Colin Kho(HP) <colinzkho@hotmail.com>
2013-06-01 19:43:45 -0700

    Added Mock Tests for ChatController

--- src/realtalk/controller/ChatController.java
+++ src/realtalk/controller/ChatController.java
@@ -12,6 +12,7 @@ import realtalk.util.ChatManager;
 import realtalk.util.ChatRoomInfo;
 import realtalk.util.ChatRoomResultSet;
 import realtalk.util.CommonUtilities;
+import realtalk.util.IChatManager;
 import realtalk.util.MessageInfo;
 import realtalk.util.PullMessageResultSet;
 import realtalk.util.RequestResultSet;
@@ -30,9 +31,15 @@ public final class ChatController {
     // Set to null if user not set / logged out.
     private UserInfo userinfo;
     
+    // Variables used for debugging and testing
+    private boolean debug;
+    private IChatManager cm;
+    
     private ChatController() {
         chatModel = new HallwayModel();
         this.userinfo = null;
+        this.debug = false;
+        this.cm = null;
     }
     
     /**
@@ -70,7 +77,12 @@ public final class ChatController {
      * @return True if refresh succeeded, false if otherwise.
      */
     public boolean fRefresh() {
-        ChatRoomResultSet crrs = ChatManager.crrsUsersChatrooms(userinfo);
+    	ChatRoomResultSet crrs;
+    	if (!debug) {
+    		crrs = ChatManager.crrsUsersChatrooms(userinfo);
+    	} else {
+    		crrs = cm.crrsUsersChatrooms(userinfo);
+    	}
         // Clear the current chat model.
         chatModel = null;
         chatModel = new HallwayModel();
@@ -89,7 +101,12 @@ public final class ChatController {
                     chatroom.numUsersGet(),
                     chatroom.timestampCreated());
             // Next load the messages for that chatroom.
-            PullMessageResultSet pmrsMessages = ChatManager.pmrsChatLogGet(chatroom);
+            PullMessageResultSet pmrsMessages;
+            if (!debug) {
+            	pmrsMessages = ChatManager.pmrsChatLogGet(chatroom);
+            } else {
+            	pmrsMessages = cm.pmrsChatLogGet(chatroom);
+            }
             if (!pmrsMessages.fIsSucceeded()) {
                 // Else remove all rooms and return failure.
                 chatModel = null;
@@ -150,7 +167,12 @@ public final class ChatController {
      * @return True if succeeded, false if otherwise.
      */
     public boolean joinRoom(ChatRoomInfo chatroom) {
-        RequestResultSet rrs = ChatManager.rrsJoinRoom(userinfo, chatroom);
+        RequestResultSet rrs;
+        if (!debug) {
+        	rrs = ChatManager.rrsJoinRoom(userinfo, chatroom);
+        } else {
+        	rrs = cm.rrsJoinRoom(userinfo, chatroom);
+        }
         if (!rrs.getfSucceeded()) {
             return false;
         }
@@ -162,7 +184,12 @@ public final class ChatController {
                 chatroom.stCreator(), 
                 chatroom.timestampCreated());
         // Next load the messages for that chatroom.
-        PullMessageResultSet pmrsMessages = ChatManager.pmrsChatLogGet(chatroom);
+        PullMessageResultSet pmrsMessages; 
+		if (!debug) {
+    		pmrsMessages = ChatManager.pmrsChatLogGet(chatroom);
+		} else {
+			pmrsMessages = cm.pmrsChatLogGet(chatroom);
+		}
         if (!pmrsMessages.fIsSucceeded()) {
             // Else remove room and return failure
             chatModel.fRemoveRoom(chatroom.stId());
@@ -181,7 +208,13 @@ public final class ChatController {
      * @return
      */
     public boolean leaveRoom(ChatRoomInfo chatroom) {
-        RequestResultSet crrs = ChatManager.rrsLeaveRoom(userinfo, chatroom);
+    	RequestResultSet crrs;
+    	if (!debug) {
+    		crrs = ChatManager.rrsLeaveRoom(userinfo, chatroom);
+    	} else {
+    		crrs = cm.rrsLeaveRoom(userinfo, chatroom);
+    	}
+    		
         if (!crrs.getfSucceeded()) {
             return false;
         }
@@ -204,6 +237,7 @@ public final class ChatController {
         userinfo = null;
         chatModel = null;
         chatModel = new HallwayModel();
+        cm = null;
     }
     
     /**
@@ -221,5 +255,22 @@ public final class ChatController {
             }
         }
         return false;
+    }
+    
+    /*
+     * TESTING Methods
+     */
+    
+    /**
+     * Sets the controller to be in debug mode and allows the ChatManager to be redefined.
+     * 
+     * @param fDebugMode
+     */
+    public void setDebugMode(boolean fDebugMode) {
+    	this.debug = fDebugMode;
+    }
+    
+    public void setChatManager(IChatManager cm) {
+    	this.cm = cm;
     }
 }
