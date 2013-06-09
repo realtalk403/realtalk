4f1b9c5bfd4ad46e120a7bb5f6ace258953fdead
Jory Rice <thisisjory@gmail.com>
2013-06-09 16:24:47 -0700

    fixed auto login bug. booyah

--- src/realtalk/util/HttpUtility.java
+++ src/realtalk/util/HttpUtility.java
@@ -24,6 +24,9 @@ import org.apache.http.params.HttpParams;
  *
  */
 public final class HttpUtility {
+	
+	private static final int CONNECTION_TIMEOUT = 30000;
+	
 	/**
 	 * This method makes a Get Request given a list of params and retrieves the response
 	 * from the server. It returns the response as an inputStream.
@@ -37,7 +40,7 @@ public final class HttpUtility {
 	 */
 	public static InputStream sendGetRequest(String stUrl, List<NameValuePair> rgparams) 
 			throws IOException, UnsupportedOperationException, ClientProtocolException {
-		DefaultHttpClient httpclient = new DefaultHttpClient();
+        DefaultHttpClient httpclient = defaulthttpclientCreateClient();
         String stParam = URLEncodedUtils.format(rgparams, "utf-8");
         stUrl += "?" + stParam;
         HttpGet httpget = new HttpGet(stUrl);
@@ -60,10 +63,7 @@ public final class HttpUtility {
 	 */
 	public static InputStream sendPostRequest(String stUrl, List<NameValuePair> rgparams) 
 			throws IOException, UnsupportedOperationException, ClientProtocolException {
-        final HttpParams httpParams = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(httpParams, 30000);
-        
-		DefaultHttpClient httpclient = new DefaultHttpClient(httpParams);
+        DefaultHttpClient httpclient = defaulthttpclientCreateClient();
         HttpPost httppost = new HttpPost(stUrl);
         httppost.setEntity(new UrlEncodedFormEntity(rgparams, "UTF-8"));
 
@@ -72,6 +72,16 @@ public final class HttpUtility {
         return httpentity.getContent();
 	}
 	
+	/**
+	 * Creates a default http client with a connection timeout paramter.
+	 * @return default http client
+	 */
+	private static DefaultHttpClient defaulthttpclientCreateClient() {
+		final HttpParams httpParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, CONNECTION_TIMEOUT);
+		return new DefaultHttpClient(httpParams);
+	}
+
 	/*
 	 * Private Constructor that throws an error if it is used. Also this disallows the public constructor.
 	 */
